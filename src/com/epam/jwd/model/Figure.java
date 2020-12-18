a30f05a43e96d4a9f79d7453febb74d8aee2b8d5
projectX_BSUIR <degtyarev9482@gmail.com>
2020-12-18 16:37:26 +0300

    changed getType function

--- src/com/epam/jwd/model/Figure.java
+++ src/com/epam/jwd/model/Figure.java
@@ -1,5 +1,6 @@
 package com.epam.jwd.model;
 
+import com.epam.jwd.exception.StrategyNotSetException;
 import com.epam.jwd.strategy.Strategy;
 
 public abstract class Figure {
@@ -7,37 +8,56 @@ public abstract class Figure {
 
     protected Color figureColor;
 
-    private Strategy figurePropertiesStrategy;
+    protected final int id;
 
-    Figure(){
-        setFigureColor(Color.BLUE);
-        figurePropertiesStrategy = null;
+    protected FigureType type;
+
+    private static int figureCount;
+
+    static {
+        figureCount = 1;
     }
 
-    public final void setFigurePropertiesStrategy(Strategy strategy){
+    private Strategy figurePropertiesStrategy;
+
+    Figure() {
+        setFigureColor(Color.BLUE);
+        figurePropertiesStrategy = null;
+        id = figureCount++;
+    }
+
+    public final void setFigurePropertiesStrategy(Strategy strategy) {
         this.figurePropertiesStrategy = strategy;
     }
 
-    public final double execute (){
+    public final double execute() {
         try {
             return figurePropertiesStrategy.calculate(this.points);
-        }catch (NullPointerException e){
-            throw new NullPointerException("you have to choose strategy before executing!");
+        } catch (NullPointerException e) {
+            throw new StrategyNotSetException("you have to choose strategy before executing!");
         }
     }
 
-    public void setPoints(Point[] points){
+    public void setPoints(Point[] points) {
         this.points = points;
-    };
+    }
+
+
     public abstract boolean pointsMatch();
-    public abstract String getType();
+
+    public abstract FigureType getType();
+
     public abstract boolean isPossibleToBuild();
 
-    public void setFigureColor(Color figureColor){
+    public void setFigureColor(Color figureColor) {
         this.figureColor = figureColor;
     }
 
     public Color getFigureColor() {
         return figureColor;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- src/com/epam/jwd/model/FigureCriteria.java
+++ src/com/epam/jwd/model/FigureCriteria.java
@@ -1,25 +1,99 @@
 package com.epam.jwd.model;
 
-public class FigureCriteria {
-    private FigureType figureType;
-    private Color figureColor;
-    private double bottomSquareLimit;
 
-    public FigureCriteria(FigureType figureType, Color figureColor, double bottomSquareLimit) {
+import java.util.Optional;
+
+public class FigureCriteria {
+    private final FigureType figureType;
+    private final Color figureColor;
+    private final Double bottomSquareLimit;
+    private final Integer figureId;
+    private final Integer bottomIdBorder;
+    private final Integer topIdBorder;
+
+    private FigureCriteria(FigureType figureType,
+                           Color figureColor,
+                           Double bottomSquareLimit,
+                           Integer figureId,
+                           Integer bottomRangeIdBorder,
+                           Integer topRangeIdBorder) {
         this.figureType = figureType;
         this.figureColor = figureColor;
         this.bottomSquareLimit = bottomSquareLimit;
+        this.figureId = figureId;
+        this.bottomIdBorder = bottomRangeIdBorder;
+        this.topIdBorder = topRangeIdBorder;
     }
 
-    public Color getFigureColor() {
-        return figureColor;
+    public Optional<Color> getFigureColor() {
+        return Optional.ofNullable(figureColor);
     }
 
-    public double getBottomSquareLimit() {
-        return bottomSquareLimit;
+    public Optional<Double> getBottomSquareLimit() {
+        return Optional.ofNullable(bottomSquareLimit);
     }
 
-    public FigureType getFigureType(){
-        return figureType;
+    public Optional<FigureType> getFigureType() {
+        return Optional.ofNullable(figureType);
+    }
+
+    public Optional<Integer> getFigureId(){
+        return Optional.ofNullable(figureId);
+    }
+
+    public Optional<Integer[]> getRangId(){
+        if(bottomIdBorder == null && topIdBorder == null){
+            return Optional.empty();
+        }else{
+            return Optional.of(new Integer[]{bottomIdBorder, topIdBorder});
+        }
+    }
+
+
+
+
+
+    public static class CriteriaBuilder{
+
+        private FigureType figureType;
+        private Color figureColor;
+        private Double bottomSquareLimit;
+        private Integer figureId;
+        private Integer bottomIdBorder;
+        private Integer topIdBorder;
+
+        public CriteriaBuilder setFigureType(FigureType figureType) {
+            this.figureType = figureType;
+            return this;
+        }
+
+
+        public CriteriaBuilder setFigureColor(Color figureColor) {
+            this.figureColor = figureColor;
+            return this;
+        }
+
+
+        public CriteriaBuilder setBottomSquareLimit(double squareLimit) {
+            this.bottomSquareLimit = squareLimit;
+            return this;
+        }
+
+        public CriteriaBuilder setFigureId(int figureId){
+            this.figureId = figureId;
+            return this;
+        }
+
+        public CriteriaBuilder setIdRange(int bottomIdBorder, int topIdBorder){
+            this.bottomIdBorder = bottomIdBorder;
+            this.topIdBorder = topIdBorder;
+            return this;
+        }
+
+        public FigureCriteria getCriteria(){
+
+            return new FigureCriteria(figureType, figureColor, bottomSquareLimit, figureId, bottomIdBorder, topIdBorder);
+        }
+
     }
 }
