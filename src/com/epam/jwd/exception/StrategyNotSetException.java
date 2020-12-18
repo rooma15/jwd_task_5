f4523399f296110d40d8b88b7ccf11fde2032b99
projectX_BSUIR <degtyarev9482@gmail.com>
2020-12-18 16:34:04 +0300

    added exception

--- src/com/epam/jwd/exception/StrategyNotSetException.java
+++ src/com/epam/jwd/exception/StrategyNotSetException.java
@@ -1,2 +1,10 @@
-package com.epam.jwd.exception;public class StrategyNotSetException {
+package com.epam.jwd.exception;
+
+public class StrategyNotSetException extends StrategyException{
+    public StrategyNotSetException() {
+    }
+
+    public StrategyNotSetException(String message) {
+        super(message);
+    }
 }
